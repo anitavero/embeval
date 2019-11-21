9a8103e648b25e055213e114ff9c8a97fad1c638
Anita Vero <alv34@cam.ac.uk>
2019-11-21 20:37:07 +0000

    Fix processing pickled vecs.

--- process_embeddings.py
+++ process_embeddings.py
@@ -17,14 +17,14 @@ def agg_img_embeddings(filepath, savedir, maxnum=10):
     filename = os.path.basename(filepath).split('.')[0]
 
     with open(os.path.join(savedir, filename + '.vocab'), 'w') as f:
-        f.write('\n'.join([str(s) for s in data_dict.keys()]))
+        f.write('\n'.join([str(s, 'utf-8') for s in data_dict.keys()]))
 
 
     # Aggregate image vectors for a word, using the fist min(maxnum, imangenum) images
     embeddings = np.empty((len(data_dict), np.array(list(list(data_dict.values())[0].values())).shape[1]))
     for i, imgs in enumerate(tqdm(data_dict.values())):
         vecs = np.array(list(imgs.values()))
-        embeddings[i] = vecs[:min(maxnum, vecs.shape[0])].mean(axis=1)
+        embeddings[i] = vecs[:min(maxnum, vecs.shape[0])].mean(axis=0)
 
     np.save(os.path.join(savedir, filename + '.npy'), embeddings)
 
