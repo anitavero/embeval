85ff1144939fab9bfc3fd5303da83a94c8a73616
Anita Vero <alv34@cam.ac.uk>
2019-11-27 23:24:40 +0000

    create_index_from_fnames.

--- source/cnn_embeddings.py
+++ source/cnn_embeddings.py
@@ -8,11 +8,12 @@ from tqdm import tqdm
 import argh
 from argh import arg
 from collections import defaultdict
+from glob import glob
 
 from process_embeddings import serialize2npy
 
-
-@arg('-cnn', '--cnn_model', choices=['alexnet', 'resnet-18', 'googlenet'], default='resnet-18')   # TODO
+# TODO: more models
+@arg('-cnn', '--cnn_model', choices=['alexnet', 'resnet-18', 'googlenet'], default='resnet-18')
 def get_cnn(image_dir, word_index_file, savedir=None, cnn_model='resnet', agg_maxnum=10):
     """Extract CNN representations for images in a directory and saves it into a dictionary file."""
     img2vec = Img2Vec(model=cnn_model)
@@ -38,6 +39,17 @@ def get_cnn(image_dir, word_index_file, savedir=None, cnn_model='resnet', agg_ma
 
     # Save aggregated embeddings
     serialize2npy(repr_path, savedir, agg_maxnum)
+
+
+def create_index_from_fnames(image_dir, savepath):
+    files = glob(image_dir)
+    word_index_file = defaultdict(list)
+    for f in files:
+        fname = os.path.basename(f)
+        word = fname.split('_')[0]
+        word_index_file[word].append(fname)
+    with open(savepath, 'wb') as f:
+        json.dump(word_index_file, f)
 
 
 if __name__ == '__main__':
