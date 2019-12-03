b122d13d8ca105227ca2a3988fa488623cfab475
Anita Vero <alv34@cam.ac.uk>
2019-12-03 14:04:52 +0000

    Add unzip_fasttext.sh.

---
+++ source/unzip_fasttext.sh
@@ -0,0 +1,6 @@
+DIR=$1
+
+unzip "$DIR/wiki-news-300d-1M.vec.zip" -d $DIR
+unzip "$DIR/wiki-news-300d-1M-subword.vec.zip" -d $DIR
+unzip "$DIR/crawl-300d-2M.vec.zip" -d $DIR
+unzip "$DIR/crawl-300d-2M-subword.zip" -d $DIR
