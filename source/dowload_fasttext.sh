16b54be71b3217cbec271f7b62f243be27178ab2
Anita Vero <alv34@cam.ac.uk>
2019-12-03 12:29:25 +0000

    Comment dowload_fasttext.sh

--- source/dowload_fasttext.sh
+++ source/dowload_fasttext.sh
@@ -1,3 +1,11 @@
+
+# Pre-trained word vectors learned on different sources are downloaded below:
+#
+# wiki-news-300d-1M.vec.zip: 1 million word vectors trained on Wikipedia 2017, UMBC webbase corpus and statmt.org news dataset (16B tokens).
+# wiki-news-300d-1M-subword.vec.zip: 1 million word vectors trained with subword infomation on Wikipedia 2017, UMBC webbase corpus and statmt.org news dataset (16B tokens).
+# crawl-300d-2M.vec.zip: 2 million word vectors trained on Common Crawl (600B tokens).
+# crawl-300d-2M-subword.zip: 2 million word vectors trained with subword information on Common Crawl (600B tokens).
+
 DIR=$1
 
 # -N will download and overwrite the file only if the server has a newer version.
