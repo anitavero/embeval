66c17d5560902292ea85bf8e751ef29faf963a61
Anita Vero <alv34@cam.ac.uk>
2020-04-05 20:49:20 +0200

    Log loss curve during training.

--- embedding.py
+++ embedding.py
@@ -1,31 +1,54 @@
 import os
 from itertools import tee
 import logging
+import json
 from gensim.models import Word2Vec
 from gensim.models.callbacks import CallbackAny2Vec
+from matplotlib import pyplot as plt
 
 logger = logging.getLogger(__name__)
 
 
-class EpochLogger(CallbackAny2Vec):
+class LossLogger(CallbackAny2Vec):
     """Callback to print loss after each epoch."""
 
-    def __init__(self):
+    def __init__(self, show=False):
+        """
+        :param show: If True, show loss curve in the end.
+        """
         self.epoch = 0
+        self.prev_cum_loss = 0
+        self.batch_losses = []  # loss after each batch, for every epoch (?)
+        self.epoch_losses = []
+        self.show = show
 
     def on_epoch_begin(self, model):
-        loss = model.get_latest_training_loss()
-        print("Epoch #{} start, loss: {}".format(self.epoch, loss))
+        self.batch = 0
 
     def on_epoch_end(self, model):
-        loss = model.get_latest_training_loss()
-        print("Epoch #{} end, loss: {}".format(self.epoch, loss))
+        cum_loss = model.get_latest_training_loss()
+        eloss = cum_loss - self.prev_cum_loss
+        print("Epoch #{} end, loss: {}".format(self.epoch, eloss))
         self.epoch += 1
+        self.epoch_losses.append(eloss)
+
+    def on_batch_end(self, model):
+        cum_loss = model.get_latest_training_loss()
+        loss = abs(cum_loss - self.prev_cum_loss)
+        print(f"Epoch {self.epoch} - Batch {self.batch} end loss: {loss}")
+        self.prev_cum_loss = cum_loss
+        self.batch_losses.append(loss)
+        self.batch += 1
+
+    def on_train_end(self, model):
+        plt.plot(self.batch_losses)
+        if self.show:
+            plt.show()
 
 
 def train(corpus, save_path, load_path=None,
           size=300, window=5, min_count=10, workers=4,
-          epochs=5, max_vocab_size=None):
+          epochs=5, max_vocab_size=None, show_loss=False):
     """
     Train w2v.
     :param corpus: list of list strings
@@ -35,7 +58,7 @@ def train(corpus, save_path, load_path=None,
     texts, texts_build, texts_l = tee(corpus, 3)
     total_examples = len(list(texts_l))
 
-    epoch_logger = EpochLogger()
+    loss_logger = LossLogger(show_loss)  # TODO: loss curve looks weird with multiple workers
 
     if not os.path.exists(save_path) and load_path is None:
         model = Word2Vec(texts_build, size=size, window=window, min_count=min_count, workers=workers,
@@ -48,10 +71,12 @@ def train(corpus, save_path, load_path=None,
         model.build_vocab(texts_build, update=True)
         logger.debug('Updates vocab, new size: {}'.format(len(model.wv.vocab)))
 
-    model.train(texts, total_examples=total_examples, epochs=epochs, callbacks=[epoch_logger], compute_loss=True)
-    print('Loss after training:', model.get_latest_training_loss())
+    model.train(texts, total_examples=total_examples, epochs=epochs, callbacks=[loss_logger], compute_loss=True)
 
     print('Saving model')
     model.save(save_path)
+    plt.savefig(save_path + '_losscurve.png')
+    with open(save_path + '_losscurve.json', 'w') as f:
+        json.dump(loss_logger.batch_losses, f)
 
     return model
