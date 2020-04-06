dd5d4bff8f34f83e43562c7817b820272cdcd9ad
Anita Vero <alv34@cam.ac.uk>
2020-04-06 13:26:56 +0200

    Rename module calls in run_experiments.

--- source/run_experiments.py
+++ source/run_experiments.py
@@ -1,4 +1,4 @@
-import eval
+import task_eval
 import argh
 
 
@@ -9,99 +9,99 @@ def main(exp_name):
     # savedir=embdir
     #
     # if exp_name == 'nopadding_mitchell':
-    #     eval.main(datadir, actions=['compbrain'], embdir=embdir,
-    #               vecs_names=['fmri-internal_m5_mm_descriptors'],
-    #               ling_vecs_names=['w2v13'],
-    #               mm_lingvis=True,
-    #               mm_padding=False,
-    #               savepath=savedir + 'test_mitchell')
+    #     task_eval.main(datadir, actions=['compbrain'], embdir=embdir,
+    #                    vecs_names=['fmri-internal_m5_mm_descriptors'],
+    #                    ling_vecs_names=['w2v13'],
+    #                    mm_lingvis=True,
+    #                    mm_padding=False,
+    #                    savepath=savedir + 'test_mitchell')
     ######## END Test #######
 
-    datadir='/local/filespace/alv34/Datasets/'
-    embdir='/local/filespace/alv34/mmdeed/'
-    savedir=embdir + '/results/'
+    datadir = '/local/filespace/alv34/Datasets/'
+    embdir = '/local/filespace/alv34/embeval/'
+    savedir = embdir + '/results/'
 
     if exp_name == 'padding':
-        eval.main(datadir, actions=['compscores', 'compbrain'], embdir=embdir,
-                  vecs_names=['google_alexnetfc7', 'vecs3lem1', 'google_vgg',
-                              'men-internal', 'men-whole', 'google_resnet152'],
-                  ling_vecs_names=['wikinews', 'wikinews_sub', 'crawl', 'w2v13'],
-                  mm_lingvis=True,
-                  mm_padding=True,
-                  savepath=savedir + exp_name)
+        task_eval.main(datadir, actions=['compscores', 'compbrain'], embdir=embdir,
+                       vecs_names=['google_alexnetfc7', 'vecs3lem1', 'google_vgg',
+                                   'men-internal', 'men-whole', 'google_resnet152'],
+                       ling_vecs_names=['wikinews', 'wikinews_sub', 'crawl', 'w2v13'],
+                       mm_lingvis=True,
+                       mm_padding=True,
+                       savepath=savedir + exp_name)
 
     if exp_name == 'nopadding':
-        eval.main(datadir, actions=['compscores', 'compbrain'], embdir=embdir,
-                  vecs_names=['google_alexnetfc7', 'vecs3lem1', 'google_vgg',
-                              'men-internal', 'men-whole', 'google_resnet152'],
-                  ling_vecs_names=['wikinews', 'wikinews_sub', 'crawl', 'w2v13'],
-                  mm_lingvis=True,
-                  mm_padding=False,
-                  savepath=savedir + exp_name)
+        task_eval.main(datadir, actions=['compscores', 'compbrain'], embdir=embdir,
+                       vecs_names=['google_alexnetfc7', 'vecs3lem1', 'google_vgg',
+                                   'men-internal', 'men-whole', 'google_resnet152'],
+                       ling_vecs_names=['wikinews', 'wikinews_sub', 'crawl', 'w2v13'],
+                       mm_lingvis=True,
+                       mm_padding=False,
+                       savepath=savedir + exp_name)
 
     # if exp_name == 'nopadding_common_subset':
-    #     eval.main(datadir, actions=['compscores', 'compbrain'], embdir=embdir,
-    #               vecs_names=['google_alexnetfc7', 'vecs3lem1', 'google_vgg',
-    #                           'men-internal', 'men-whole', 'google_resnet152'],
-    #               ling_vecs_names=['wikinews', 'wikinews_sub', 'crawl', 'w2v13'],
-    #               mm_lingvis=True,
-    #               mm_padding=False,
-    #               savepath=savedir + exp_name)
+    #     task_eval.main(datadir, actions=['compscores', 'compbrain'], embdir=embdir,
+    #                    vecs_names=['google_alexnetfc7', 'vecs3lem1', 'google_vgg',
+    #                                'men-internal', 'men-whole', 'google_resnet152'],
+    #                    ling_vecs_names=['wikinews', 'wikinews_sub', 'crawl', 'w2v13'],
+    #                    mm_lingvis=True,
+    #                    mm_padding=False,
+    #                    savepath=savedir + exp_name)
 
 
     ######## Brain experiments ########
 
     if exp_name == 'padding_mitchell':
-        eval.main(datadir, actions=['compbrain'], embdir=embdir,
-                  vecs_names=['fmri_google_resnet152', 'fmri_google_alexnet',
-                              'fmri-internal_m5_vs_descriptors', 'fmri-internal_m5_mm_descriptors',
-                              'fmri_combined_vs_descriptors', 'fmri_combined_mm_descriptors',
-                              'frcnn_whole_vs_descriptors', 'frcnn_whole_mm_descriptors',
-                              'vecs3lem1'],
-                  ling_vecs_names=['wikinews', 'wikinews_sub', 'crawl', 'w2v13'],
-                  mm_lingvis=True,
-                  mm_padding=True,
-                  savepath=savedir + exp_name)
+        task_eval.main(datadir, actions=['compbrain'], embdir=embdir,
+                       vecs_names=['fmri_google_resnet152', 'fmri_google_alexnet',
+                                   'fmri-internal_m5_vs_descriptors', 'fmri-internal_m5_mm_descriptors',
+                                   'fmri_combined_vs_descriptors', 'fmri_combined_mm_descriptors',
+                                   'frcnn_whole_vs_descriptors', 'frcnn_whole_mm_descriptors',
+                                   'vecs3lem1'],
+                       ling_vecs_names=['wikinews', 'wikinews_sub', 'crawl', 'w2v13'],
+                       mm_lingvis=True,
+                       mm_padding=True,
+                       savepath=savedir + exp_name)
 
     if exp_name == 'nopadding_mitchell':
-        eval.main(datadir, actions=['compbrain'], embdir=embdir,
-                  vecs_names=['fmri_google_resnet152', 'fmri_google_alexnet',
-                              'fmri-internal_m5_vs_descriptors', 'fmri-internal_m5_mm_descriptors',
-                              'fmri_combined_vs_descriptors', 'fmri_combined_mm_descriptors',
-                              'frcnn_whole_vs_descriptors', 'frcnn_whole_mm_descriptors',
-                              'vecs3lem1'],
-                  ling_vecs_names=['wikinews', 'wikinews_sub', 'crawl', 'w2v13'],
-                  mm_lingvis=True,
-                  mm_padding=False,
-                  savepath=savedir + exp_name)
+        task_eval.main(datadir, actions=['compbrain'], embdir=embdir,
+                       vecs_names=['fmri_google_resnet152', 'fmri_google_alexnet',
+                                   'fmri-internal_m5_vs_descriptors', 'fmri-internal_m5_mm_descriptors',
+                                   'fmri_combined_vs_descriptors', 'fmri_combined_mm_descriptors',
+                                   'frcnn_whole_vs_descriptors', 'frcnn_whole_mm_descriptors',
+                                   'vecs3lem1'],
+                       ling_vecs_names=['wikinews', 'wikinews_sub', 'crawl', 'w2v13'],
+                       mm_lingvis=True,
+                       mm_padding=False,
+                       savepath=savedir + exp_name)
 
     ##### Common subset #####
 
     if exp_name == 'padding_mitchell_commonsubset':
-        eval.main(datadir, actions=['compbrain'], embdir=embdir,
-                  vecs_names=['fmri_google_resnet152', 'fmri_google_alexnet',
-                              'fmri-internal_m5_vs_descriptors', 'fmri-internal_m5_mm_descriptors',
-                              'fmri_combined_vs_descriptors', 'fmri_combined_mm_descriptors',
-                              'frcnn_whole_vs_descriptors', 'frcnn_whole_mm_descriptors',
-                              'vecs3lem1'],
-                  ling_vecs_names=['wikinews', 'wikinews_sub', 'crawl', 'w2v13'],
-                  mm_lingvis=True,
-                  mm_padding=True,
-                  savepath=savedir + exp_name,
-                  common_subset=True)
+        task_eval.main(datadir, actions=['compbrain'], embdir=embdir,
+                       vecs_names=['fmri_google_resnet152', 'fmri_google_alexnet',
+                                   'fmri-internal_m5_vs_descriptors', 'fmri-internal_m5_mm_descriptors',
+                                   'fmri_combined_vs_descriptors', 'fmri_combined_mm_descriptors',
+                                   'frcnn_whole_vs_descriptors', 'frcnn_whole_mm_descriptors',
+                                   'vecs3lem1'],
+                       ling_vecs_names=['wikinews', 'wikinews_sub', 'crawl', 'w2v13'],
+                       mm_lingvis=True,
+                       mm_padding=True,
+                       savepath=savedir + exp_name,
+                       common_subset=True)
 
     if exp_name == 'nopadding_mitchell_commonsubset':
-        eval.main(datadir, actions=['compbrain'], embdir=embdir,
-                  vecs_names=['fmri_google_resnet152', 'fmri_google_alexnet',
-                              'fmri-internal_m5_vs_descriptors', 'fmri-internal_m5_mm_descriptors',
-                              'fmri_combined_vs_descriptors', 'fmri_combined_mm_descriptors',
-                              'frcnn_whole_vs_descriptors', 'frcnn_whole_mm_descriptors',
-                              'vecs3lem1'],
-                  ling_vecs_names=['wikinews', 'wikinews_sub', 'crawl', 'w2v13'],
-                  mm_lingvis=True,
-                  mm_padding=False,
-                  savepath=savedir + exp_name,
-                  common_subset=True)
+        task_eval.main(datadir, actions=['compbrain'], embdir=embdir,
+                       vecs_names=['fmri_google_resnet152', 'fmri_google_alexnet',
+                                   'fmri-internal_m5_vs_descriptors', 'fmri-internal_m5_mm_descriptors',
+                                   'fmri_combined_vs_descriptors', 'fmri_combined_mm_descriptors',
+                                   'frcnn_whole_vs_descriptors', 'frcnn_whole_mm_descriptors',
+                                   'vecs3lem1'],
+                       ling_vecs_names=['wikinews', 'wikinews_sub', 'crawl', 'w2v13'],
+                       mm_lingvis=True,
+                       mm_padding=False,
+                       savepath=savedir + exp_name,
+                       common_subset=True)
 
 
 if __name__ == '__main__':
