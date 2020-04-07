89593b12d9821c803010b1bf87cb4615a4af3ef1
Anita Vero <alv34@cam.ac.uk>
2020-04-07 17:51:28 +0200

    Remove duplicated test code.

--- source/run_infogain_experiments.py
+++ source/run_infogain_experiments.py
@@ -9,17 +9,6 @@ def main(exp_name):
     # datadir = '/Users/anitavero/projects/data'
     # embdir = '/Users/anitavero/projects/data/wikidump/models/'
     # savedir = embdir
-    #
-    # quantity_models = glob(embdir + '*model*')
-    # quantity_models = [os.path.split(m)[1] for m in quantity_models if '.' not in m]
-    #
-    # if exp_name == 'quantity':
-    #     task_eval.main(datadir, actions=['compscores'], embdir=embdir,
-    #                    vecs_names=quantity_models,
-    #                    ling_vecs_names=[],
-    #                    mm_lingvis=False,
-    #                    mm_padding=False,
-    #                    savepath=savedir + exp_name)
     ######## END Test #######
 
     datadir = '/local/filespace/alv34/Datasets/'
