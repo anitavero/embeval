2de2261808cec09315114e04d3fbbc279b8c3cf7
Anita Vero <alv34@cam.ac.uk>
2020-08-01 01:26:03 +0200

    Remove unused lines. Docstring.

--- source/emb_information.py
+++ source/emb_information.py
@@ -6,7 +6,6 @@ Analytical vs estimated value is illustrated for normal random variables.
 
 """
 import os
-import numpy as np
 from numpy.random import rand, multivariate_normal
 from numpy import array, arange, zeros, dot, ones, sum
 import matplotlib
@@ -16,7 +15,6 @@ import matplotlib.pyplot as plt
 from time import time
 import argh
 from argh import arg
-from typing import List
 
 from ite.cost.x_factory import co_factory
 from ite.cost.x_analytical_values import analytical_value_i_shannon
@@ -100,21 +98,14 @@ def benchmark(dim=10, cost_name='MIShannon_DKL', num_of_samples=-1, max_num_of_s
         plt.savefig(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'figs', fig_title))
 
 
-def estimate_mi(matrices: List[np.ndarray], cost_name: str):
-    """Return estimated Mutual Information for a list of matrices.
-        :param matrices: array of numpy matrices
-        :param cost_name: MI estimation algorithm, e.g, 'BIHSIC_IChol', 'MIShannon_DKL', 'MIShannon_HS' (for more see ite.cost)
-    """
-    co = co_factory(cost_name, mult=True)  # cost object
-    ds = [m.shape[1] for m in matrices]
-    ms_stacked = np.hstack(matrices)
-    return co.estimation(ms_stacked, ds)
-
-
 @arg('-mmembs', '--mm_embs_of', type=tuple_list)
 @arg('-vns', '--vecs_names', nargs='+', type=str, required=True)
 def estimate_embeddings_mi(datadir: str, vecs_names=[], mm_embs_of=[], cost_name='MIShannon_DKL'):
     """Return estimated Mutual Information for a Embeddings with vecs_names in datadir.
+        :param datadir: Path to directory which contains embedding data.
+        :param vecs_names: List[str] Names of embeddings
+        :param mm_embs_of: List of str tuples, where the tuples contain names of embeddings which are to
+                       be concatenated into a multi-modal mid-fusion embedding.
         :param cost_name: MI estimation algorithm, e.g, 'BIHSIC_IChol', 'MIShannon_DKL', 'MIShannon_HS' (for more see ite.cost)
     """
     embs = Embeddings(datadir, vecs_names)
