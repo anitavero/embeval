2df99e164663788e8b571daf7960d6727efdbf23
Anita Vero <alv34@cam.ac.uk>
2020-07-31 20:29:32 +0200

    Estimate MI of Embeddings.

--- source/emb_information.py
+++ source/emb_information.py
@@ -6,6 +6,7 @@ Analytical vs estimated value is illustrated for normal random variables.
 
 """
 import os
+import numpy as np
 from numpy.random import rand, multivariate_normal
 from numpy import array, arange, zeros, dot, ones, sum
 import matplotlib
@@ -14,11 +15,14 @@ if os.environ.get('DISPLAY') is None:
 import matplotlib.pyplot as plt
 from time import time
 import argh
+from argh import arg
+from typing import List
 
 from ite.cost.x_factory import co_factory
 from ite.cost.x_analytical_values import analytical_value_i_shannon
 
 from source.utils import hr_time
+from source.process_embeddings import Embeddings
 
 
 def benchmark(dim=10, cost_name='MIShannon_DKL', num_of_samples=-1, max_num_of_samples=10000):
@@ -96,6 +100,25 @@ def benchmark(dim=10, cost_name='MIShannon_DKL', num_of_samples=-1, max_num_of_s
         plt.savefig(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'figs', fig_title))
 
 
+def estimate_mi(matrices: List[np.ndarray], cost_name: str):
+    """Return estimated Mutual Information for a list of matrices.
+        :param matrices: array of numpy matrices
+        :param cost_name: MI estimation algorithm, e.g, 'BIHSIC_IChol', 'MIShannon_DKL', 'MIShannon_HS' (for more see ite.cost)
+    """
+    co = co_factory(cost_name, mult=True)  # cost object
+    ds = [m.shape[1] for m in matrices]
+    ms_stacked = np.hstack(matrices)
+    return co.estimation(ms_stacked, ds)
+
+
+@arg('-vns', '--vecs_names', nargs='+', type=str, required=True)
+def estimate_embeddings_mi(datadir: str, vecs_names=[], cost_name='MIShannon_DKL'):
+    """Return estimated Mutual Information for a Embeddings with vecs_names in datadir.
+        :param cost_name: MI estimation algorithm, e.g, 'BIHSIC_IChol', 'MIShannon_DKL', 'MIShannon_HS' (for more see ite.cost)
+    """
+    embs = Embeddings(datadir, vecs_names)
+    return estimate_mi(embs.embeddings, cost_name)
+
 
 if __name__ == "__main__":
-    argh.dispatch_commands([benchmark])
+    argh.dispatch_commands([benchmark, estimate_embeddings_mi])
