ef8072d5d94395fd8095ef44db17b8c43260a2c5
Anita Vero <alv34@cam.ac.uk>
2020-10-05 13:53:10 +0100

    Subdir param in run infogain experiments.

--- source/run_infogain_experiments.py
+++ source/run_infogain_experiments.py
@@ -4,7 +4,7 @@ import argh
 from glob import glob
 
 
-def main(exp_name, filter_pattern='', pre_score_files=None):
+def main(exp_name, filter_pattern='', pre_score_files=None, subdir=None):
   #  ######## Test #######
   #  datadir = '/Users/anitavero/projects/data'
   #  embdir = '/Users/anitavero/projects/data/wikidump/models/'
